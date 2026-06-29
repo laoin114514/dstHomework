1d966009d3f49c55caa97fef9ec87beecd9c16b6
laoin <2908451607@qq.com>
2026-06-29 20:08:38 +0800

    feat: 添加 MainApp 主窗口整合

--- src/main/java/com/mapcoloring/Main.java
+++ src/main/java/com/mapcoloring/Main.java
@@ -1,7 +1,10 @@
 package com.mapcoloring;
 
+import com.mapcoloring.ui.MainApp;
+import javafx.application.Application;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Map Coloring Application");
+        Application.launch(MainApp.class, args);
     }
 }
