9f2275aca05dfc3357c745c000f740476c163843
laoin <2908451607@qq.com>
2026-07-02 16:15:57 +0800

    feat: ColoringResult 新增 steps 字段支持动画播放

--- src/main/java/com/mapcoloring/algorithm/ColoringResult.java
+++ src/main/java/com/mapcoloring/algorithm/ColoringResult.java
@@ -1,12 +1,24 @@
 package com.mapcoloring.algorithm;
 
+import com.mapcoloring.ui.ColoringStep;
+import java.util.List;
+
 public class ColoringResult {
     public int colorCount;
     public long timeNanos;
+    public List<ColoringStep> steps;  // 新增：步骤列表，瞬间模式为 null
 
     public ColoringResult(int colorCount, long timeNanos) {
         this.colorCount = colorCount;
         this.timeNanos = timeNanos;
+        this.steps = null;
+    }
+
+    public ColoringResult(int colorCount, long timeNanos,
+                          List<ColoringStep> steps) {
+        this.colorCount = colorCount;
+        this.timeNanos = timeNanos;
+        this.steps = steps;
     }
 
     public double timeMillis() {
